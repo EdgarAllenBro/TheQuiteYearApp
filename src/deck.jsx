3fbfdb87f0c6d704377e26ab39c38595e4078269
CameronG <cjgrieder@gmail.com>
2023-07-20 10:15:43 -0600

    small styling additions

--- src/deck.jsx
+++ src/deck.jsx
@@ -8,44 +8,43 @@ const  [diamonds, setDiamonds] = useState(props.deck.diamonds)
 const  [clubs, setClubs] = useState(props.deck.clubs)
 const  [spades, setSpades] = useState(props.deck.spades)
 
-const [currentCard,setCurrentCard] = useState({'suit':'blank','optionOne':'Click card to draw.'})
+const [currentCard,setCurrentCard] = useState({'suit':'blank','optionOne':'Click card to draw'})
 
 const drawCard = ()=>{
     if(Object.keys(hearts).length > 0){
-    let randomNum = Math.floor(Math.random() * Object.keys(hearts).length)
-        setCurrentCard({'suit':'hearts',...hearts[Object.keys(hearts)[randomNum]]})
-            delete hearts[Object.keys(hearts)[randomNum]]
+    let card = Math.floor(Math.random() * Object.keys(hearts).length)
+        setCurrentCard({'classes':`hearts`,...hearts[Object.keys(hearts)[card]]})
+            delete hearts[Object.keys(hearts)[card]]
 
     } else if(Object.keys(diamonds).length > 0){
-    let randomNum = Math.floor(Math.random() * Object.keys(diamonds).length)
-        setCurrentCard({'suit':'diamond',...diamonds[Object.keys(diamonds)[randomNum]]})
-            delete diamonds[Object.keys(diamonds)[randomNum]]
+    let card = Math.floor(Math.random() * Object.keys(diamonds).length)
+        setCurrentCard({'classes':`diamonds`,...diamonds[Object.keys(diamonds)[card]]})
+            delete diamonds[Object.keys(diamonds)[card]]
 
     } else if(Object.keys(clubs).length > 0){
-    let randomNum = Math.floor(Math.random() * Object.keys(clubs).length)
-        setCurrentCard({'suit':'clubs',...clubs[Object.keys(clubs)[randomNum]]})
-            delete clubs[Object.keys(clubs)[randomNum]]
+    let card = Math.floor(Math.random() * Object.keys(clubs).length)
+        setCurrentCard({'classes':`clubs`,...clubs[Object.keys(clubs)[card]]})
+            delete clubs[Object.keys(clubs)[card]]
     } else {
-    let randomNum = Math.floor(Math.random() * Object.keys(spades).length)
-        setCurrentCard({'suit':'spades',...spades[Object.keys(spades)[randomNum]]})
-        if(Object.keys(spades)[randomNum] === 'king'){
+    let card = Math.floor(Math.random() * Object.keys(spades).length)
+        setCurrentCard({'classes':`spades`,...spades[Object.keys(spades)[card]]})
+        if(Object.keys(spades)[card] === 'king'){
             setSpades({})
         }
-            delete spades[Object.keys(spades)[randomNum]]
+            delete spades[Object.keys(spades)[card]]
     }
+    console.log(currentCard)
 }
 if(Object.keys(spades).length === 0){
     return  (<p>{currentCard.optionOne}</p>)
 }
 return (
     <>
-<div onClick={drawCard} className={currentCard.suit} id="currentCard">
+<div onClick={drawCard} className={currentCard.classes} id="currentCard">
 <p>{currentCard.optionOne}</p>
 <p hidden={!currentCard.optionTwo}>Or</p>
 <p>{currentCard.optionTwo}</p>
 </div>
-
-
     </>
 )
 
